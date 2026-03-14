6fe4c6f562b255114a586b0014bef61ad186f1e1
jmjang88 <jmjang88@naver.com>
2026-03-14 22:51:16 +0900

    [feat] CORS 설정

---
+++ src/main/kotlin/com/sudal/storage/infrastructure/config/AppConfig.kt
@@ -0,0 +1,17 @@
+package com.sudal.storage.infrastructure.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig: WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*")
+            .exposedHeaders("Content-Length", "Content-Disposition")
+    }
+}
